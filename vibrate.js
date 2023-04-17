24f6c3f147f26f3417285e5fea48d8dbe36ae3a2
Sangram Keshari Das <102475246+sangram-keshari-das@users.noreply.github.com>
2023-04-18 00:25:57 +0530

    Add files via upload

---
+++ vibrate.js
@@ -0,0 +1,12 @@
+
+function vibrateDevice() {
+    if (navigator.vibrate) {
+      navigator.vibrate([200, 100, 200]); 
+    }
+  }
+
+alert ("Hello Guys 👑")
+
+alert ("Upvote if you like 🤩")
+
+alert ("I was thinking what to create, then I thought that I should make a cool project which everyone would like and understand. 🙂")
